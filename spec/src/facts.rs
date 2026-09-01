5084cc589ae1f0ecfbe148ae5a790e556d68423c
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-09-01 12:44:08 +0700

    fix: facts

--- spec/src/facts.rs
+++ spec/src/facts.rs
@@ -1,7 +1,23 @@
-pub fn rust_code_items_may_be_interdependent() -> bool {
-    !interdependent_code_items_examples().is_empty()
+pub fn rust_items_may_be_interdependent() -> bool {
+    !interdependent_rust_item_examples().is_empty()
 }
 
-pub fn interdependent_code_items_examples() -> &'static [&'static str] {
+pub fn interdependent_rust_item_examples() -> &'static [&'static str] {
     &[include_str!("../../fixtures/basic/src/interdependent.rs")]
+}
+
+pub fn rust_files_may_contain_meaningful_non_item_lines() -> bool {
+    !meaningful_non_item_rust_lines().is_empty()
+}
+
+pub fn meaningful_non_item_rust_lines() -> &'static [&'static str] {
+    &[
+        "#![no_std]",
+        "/// Doc comment",
+        "// Regular single-line comment",
+    ]
+}
+
+pub fn rust_analyzer_syntax_tree_retains_all_comments() -> bool {
+    true
 }
