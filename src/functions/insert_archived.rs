87e628adf40d18a673385c447f4033dfae8b8532
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-09-02 09:33:58 +0700

    fix: insert_archived

--- src/functions/insert_archived.rs
+++ src/functions/insert_archived.rs
@@ -1,8 +1,26 @@
 use crate::{Ks, Tx};
+use errgonomic::handle;
 use fjall::UserKey;
 use rkyv::Serialize;
-use rkyv::rancor::Fallible;
+use rkyv::api::high::{HighSerializer, to_bytes_in};
+use rkyv::rancor::Error as RkyvError;
+use rkyv::ser::allocator::ArenaHandle;
+use thiserror::Error;
 
-pub fn insert_archived<S: Fallible + ?Sized, K: Into<UserKey>, V: Serialize<S>>(_tx: &mut Tx, _keyspace: &Ks, _key: K, _value: V) {
-    todo!()
+pub fn insert_archived<K, V>(tx: &mut Tx<'_>, keyspace: &Ks, key: K, value: &V) -> Result<(), InsertArchivedError>
+where
+    K: Into<UserKey>,
+    V: for<'a> Serialize<HighSerializer<Vec<u8>, ArenaHandle<'a>, RkyvError>>,
+{
+    use InsertArchivedError::*;
+    let key = key.into();
+    let bytes = handle!(to_bytes_in::<_, RkyvError>(value, Vec::new()), ToBytesInFailed, key);
+    tx.insert(keyspace, key, bytes);
+    Ok(())
+}
+
+#[derive(Error, Debug)]
+pub enum InsertArchivedError {
+    #[error("failed to serialize the archived value")]
+    ToBytesInFailed { source: RkyvError, key: UserKey },
 }
